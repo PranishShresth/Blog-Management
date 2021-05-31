0a5a5da0a9aa2cec872d88446b2a042f9a2cc0a2
PranishShresth <yunche64@gmail.com>
2021-05-31 16:39:21 +1000

    login, register

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,19 +1,51 @@
-import React from "react";
-import { TextField } from "@material-ui/core";
+import React, { useState } from "react";
+import { TextField, Button, makeStyles } from "@material-ui/core";
 
+const useStyles = makeStyles((theme) => ({
+  form: {
+    "&>div": {
+      marginBottom: theme.spacing(3),
+    },
+  },
+}));
 function Login() {
+  const classes = useStyles();
+  const [loginValues, setLoginValues] = useState({ email: "", password: "" });
+  const handleChange = (ev) => {
+    setLoginValues((prevState) => ({
+      ...prevState,
+      [ev.target.id]: ev.target.value,
+    }));
+  };
+
+  const handleLogin = (ev) => {
+    ev.preventDefault();
+  };
   return (
     <div>
       <h1>Login</h1>
-      <form>
-        <TextField required id="email" type="email" label="Email" fullWidth />
+      <form className={classes.form} onSubmit={handleLogin}>
+        <TextField
+          required
+          id="email"
+          type="email"
+          label="Email"
+          fullWidth
+          value={loginValues.email}
+          onChange={handleChange}
+        />
         <TextField
           required
           id="email"
           type="password"
           label="Password"
           fullWidth
+          value={loginValues.password}
+          onChange={handleChange}
         />
+        <Button variant="contained" color="primary">
+          Login
+        </Button>
       </form>
     </div>
   );
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,9 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import { TextField, Button, makeStyles } from "@material-ui/core";
 
+const useStyles = makeStyles((theme) => ({
+  form: {
+    "&>div": {
+      marginBottom: theme.spacing(3),
+    },
+  },
+}));
 function Register() {
+  const [registerValue, setRegisterValues] = useState({
+    username: "",
+    password: "",
+    repeatPassword: "",
+    email: "",
+  });
+
+  const classes = useStyles();
+  const handleChange = (ev) => {
+    setRegisterValues((prevState) => ({
+      ...prevState,
+      [ev.target.id]: ev.target.value,
+    }));
+  };
+
+  const handleRegister = (ev) => {
+    ev.preventDefault();
+  };
   return (
     <div>
       <h1>Register</h1>
+      <form className={classes.form} onSubmit={handleRegister}>
+        <TextField
+          required
+          id="email"
+          type="email"
+          label="Email"
+          fullWidth
+          value={registerValue.email}
+          onChange={handleChange}
+        />
+        <TextField
+          required
+          id="username"
+          type="text"
+          label="Username"
+          fullWidth
+          value={registerValue.username}
+          onChange={handleChange}
+        />
+        <TextField
+          required
+          id="password"
+          type="password"
+          label="Password"
+          fullWidth
+          value={registerValue.password}
+          onChange={handleChange}
+        />
+        <TextField
+          required
+          id="repeatPassword"
+          type="password"
+          label="Repeat Password"
+          value={registerValue.repeatPassword}
+          fullWidth
+          onChange={handleChange}
+        />
+        <Button variant="contained" color="primary">
+          Register
+        </Button>
+      </form>
     </div>
   );
 }
